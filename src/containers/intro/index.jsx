c660f06b7b7dea3fdbe30fa688ba099d10e45409
s-voloshynenko <s.voloshynenko@gmail.com>
2016-07-22 01:09:55 +0300

    default colors, intro page, grid, fonts

--- src/containers/intro/index.jsx
+++ src/containers/intro/index.jsx
@@ -3,8 +3,27 @@ import React, { Component } from 'react';
 export default class Intro extends Component {
   render() {
     return (
-      <div id="intro-container">
-        {"Hello here!"}
+      <div className="row">
+        <a href="https://github.com/s-voloshynenko/js-in-the-wild">
+          <span className="logo-github">
+          </span>
+        </a>
+        <div className="col-xs-5 col-sm-5 col-md-5 col-xs-offset-3 col-sm-offset-3 col-md-offset-3" id="intro-container">
+          <h3 className="bungee-header">
+            {
+              'Hello here!'
+            }
+          </h3>
+          {
+            `This is a place, that was created
+            for the love of JavaScript. Make sure you know enough about JS.`
+          }
+          <a href="/tree">
+            <span className="intro-continue-btn">
+              Continue..
+            </span>
+          </a>
+        </div>
       </div>
     )
   }
