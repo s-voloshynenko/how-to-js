82c8ad5c1e5480ea908f5e2f44d1e97b2fd5de88
s-voloshynenko <s.voloshynenko@gmail.com>
2016-08-18 00:45:12 +0300

    horizontal tree

--- src/containers/tree/index.jsx
+++ src/containers/tree/index.jsx
@@ -47,32 +47,30 @@ const Facets = {
 export default class Tree extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-sm-7 col-md-7 col-lg-8 col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
-          <div id="tree-container">
+      <div className="row horizontal-row">
             <div className="row facets-containter lvl-1">
               <div className="center-block" style={{width: '200px'}}>
                 <Facet data={Facets.Core} />
               </div>
             </div>
             <div className="row facets-containter lvl-2">
-              <div className="col-sm-4 col-md-4 col-lg-4">
+              <div className="col-sm-12 col-md-12 col-lg-12">
                 <div className="center-block" style={{width: '200px'}}>
                   <Facet data={Facets.Client} />
                 </div>
               </div>
-              <div className="col-sm-4 col-md-4 col-lg-4">
-                <div className="center-block" style={{width: '200px'}}>
-                  <Facet data={Facets.Server} />
-                </div>
-              </div>
-              <div className="col-sm-4 col-md-4 col-lg-4">
+              <div className="col-sm-12 col-md-12 col-lg-12">
                 <div className="center-block" style={{width: '200px'}}>
                   <Facet data={Facets.Patterns} />
                 </div>
               </div>
+              <div className="col-sm-12 col-md-12 col-lg-12">
+                <div className="center-block" style={{width: '200px'}}>
+                  <Facet data={Facets.Server} />
+                </div>
+              </div>
             </div>
-            <div className="row facets-containter lvl-3">
+            <div className="row facets-containter lvl-3 lvl-client">
               <div className="col-sm-4 col-md-4 col-lg-4">
                 <div className="center-block" style={{width: '200px'}}>
                   <Facet data={Facets.ReactJS} />
@@ -84,6 +82,8 @@ export default class Tree extends Component {
                   <Facet data={Facets.AngularJS2} />
                 </div>
               </div>
+            </div>
+            <div className="row facets-containter lvl-3 lvl-server">
               <div className="col-sm-4 col-md-4 col-lg-4">
                 <div className="center-block" style={{width: '200px'}}>
                   <Facet data={Facets.Authentication} />
@@ -95,12 +95,37 @@ export default class Tree extends Component {
                   <Facet data={Facets.RabbitMQ} />
                 </div>
               </div>
-              <div className="col-sm-4 col-md-4 col-lg-4">
-              </div>
             </div>
-          </div>
-        </div>
       </div>
     )
   }
 }
+
+/**
+ <div className="row facets-containter lvl-3">
+    <div className="col-sm-4 col-md-4 col-lg-4">
+      <div className="center-block" style={{width: '200px'}}>
+        <Facet data={Facets.ReactJS} />
+      </div>
+      <div className="center-block" style={{width: '200px'}}>
+        <Facet data={Facets.AngularJS1} />
+      </div>
+      <div className="center-block" style={{width: '200px'}}>
+        <Facet data={Facets.AngularJS2} />
+      </div>
+    </div>
+    <div className="col-sm-4 col-md-4 col-lg-4">
+      <div className="center-block" style={{width: '200px'}}>
+        <Facet data={Facets.Authentication} />
+      </div>
+      <div className="center-block" style={{width: '200px'}}>
+        <Facet data={Facets.Security} />
+      </div>
+      <div className="center-block" style={{width: '200px'}}>
+        <Facet data={Facets.RabbitMQ} />
+      </div>
+    </div>
+    <div className="col-sm-4 col-md-4 col-lg-4">
+    </div>
+  </div>
+ */
