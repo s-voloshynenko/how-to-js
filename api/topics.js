c254972cd9aefd5d2f42df5f9de6b305112c4ef2
s-voloshynenko <s.voloshynenko@gmail.com>
2016-08-21 00:50:30 +0300

    topic template

--- api/topics.js
+++ api/topics.js
@@ -3,5 +3,11 @@
 module.exports = (req, res) => {
   res.send([{
     name: 'Program life cycle in the JS.'
+  }, {
+    name: 'Primitives, objects.'
+  }, {
+    name: 'Functions.'
+  }, {
+    name: 'Prototypes, OOP in JS.'
   }]);
 };
